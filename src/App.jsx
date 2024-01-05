86b1f199a017161595ff96f497de21fc9218b4d4
Ajaya Bahadur K C <kshettryaj93@gmail.com>
2024-01-05 14:39:15 +1100

    Testing rolling deployment

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
@@ -26,13 +26,14 @@ function App() {
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-        This has been deployed autmatically
+        Click on the Vite and React logos to learn more This has been deployed
+        autmatically
       </p>
 
+      <button>Application version 2</button>
       <h1>I am testing this again</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
