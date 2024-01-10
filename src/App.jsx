4cf9f2069c2a71f46d08524939aede9bc0cf1222
Ajaya Bahadur K C <kshettryaj93@gmail.com>
2024-01-10 16:10:15 +1100

    Manually tagging the docker build with git commit id

--- src/App.jsx
+++ src/App.jsx
@@ -30,7 +30,7 @@ function App() {
         autmatically
       </p>
 
-      <button>Application version 4</button>
+      <button>Application version 5</button>
       <h1>I am testing this again</h1>
     </>
   );
